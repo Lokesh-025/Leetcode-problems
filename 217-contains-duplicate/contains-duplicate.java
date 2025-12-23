f591a53568d193841a778737bdd6e73740046d2b
Lokesh <lokeshloki07070@gmail.com>
2025-12-23 12:24:26 +0530

    Time: 15 ms (61.25%) | Memory: 93.6 MB (33.05%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean containsDuplicate(int[] arr) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (set.contains(arr[i])) {
+                return true;  
+            }
+            set.add(arr[i]);
+        }
+        return false; 
+    }
+}
