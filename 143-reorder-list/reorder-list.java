60c81fa8cf21d5267c116231c94e58fa540a22ad
Lokesh <lokeshloki07070@gmail.com>
2026-02-09 21:16:49 +0530

    Time: 2 ms (89.00%) | Memory: 49.2 MB (71.28%) - LeetSync

--- 143-reorder-list/reorder-list.java
+++ 143-reorder-list/reorder-list.java
@@ -1,46 +1,36 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public void reorderList(ListNode head) {
-        if(head==null || head.next==null)
-        return;
-        ListNode slow=head;
-        ListNode fast=head;
-        while(fast.next!=null && fast.next.next!=null)
-        {
-            slow=slow.next;
-            fast=fast.next.next;
-        }
-        ListNode sec=slow.next;
-        slow.next=null;
-        ListNode prev=null;
-        while(sec!=null)
-        {
-          ListNode next=sec.next; 
-         sec.next=prev;         
-        prev=sec;             
-        sec=next; 
-        }
-        ListNode fir=head;
-        sec=prev;
-        while(sec!=null)
-        {
-            ListNode a=fir.next;
-            ListNode b=sec.next;
-            fir.next=sec;
-            sec.next=a;
-            fir=a;
-            sec=b;
-        }
-        
-        
+
+//Middle:
+       ListNode slow=head;
+       ListNode fast=head;
+       while(fast!=null && fast.next != null){
+        slow=slow.next;
+        fast=fast.next.next;
+       } 
+
+//Reverse:
+       ListNode prev=null;
+       ListNode curr=slow.next;
+       slow.next=null;
+       while(curr != null){
+        ListNode next=curr.next;
+        curr.next=prev;
+        prev=curr;
+        curr=next;
+       }
+
+//Merge:
+       ListNode first=head;
+       ListNode second=prev;
+       while(second!=null){
+        ListNode temp1=first.next;
+        ListNode temp2=second.next;
+        first.next=second;
+        second.next=temp1;
+        first=temp1;
+        second=temp2;
+       }
+
     }
 }
