6573ce517954fedf762928f4dfc48950a42713a6
Lokesh <lokeshloki07070@gmail.com>
2026-07-20 21:34:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (15.91%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,19 +1,19 @@
 class Solution {
     public int fib(int n) {
+        if(n<=1)return n;
         int a=0;
         int b=1;
-        int c=0;
         if(n==1)
         {
             return 1;
         }
-        for(int i=1;i<n;i++)
+        for(int i=2;i<=n;i++)
         {
-            c=a+b;
+            int c=a+b;
             a=b;
             b=c;
         }
-       return c;
+       return b;
 
     }
 }
